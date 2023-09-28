89bafd317e3ad9dd0b4415c94164622d09558257
Arpan Swain <kankalase07@gmail.com>
2023-09-28 17:29:57 +0530

    Added rulings

--- main.py
+++ main.py
@@ -12,17 +12,20 @@ for index, row in df.iterrows():
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, ln= 1, border=0, align = "l", txt=row["Topic"])
     pdf.line(10,21,285,21)
-
+    for y in range(20,298,10):
+        pdf.line(10,y,285,y)
     #set footer
-    pdf.ln(171)
+    pdf.ln(175)
     pdf.set_font(family="Times", size=10, style="I")
     pdf.set_text_color(180,180,180)
     pdf.cell(w=0,h=12,ln=1,align="R", txt=row["Topic"])
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 285, y)
 
-        pdf.ln(183)
+        pdf.ln(187)
         pdf.set_font(family="Times", size=10, style="I")
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=12, ln=1, align="R", txt=row["Topic"])
